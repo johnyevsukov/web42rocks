b8e5275b1bfc154551f719a54aad2e551f88fbb9
John Yevsukov <github.com@john.engineering>
2021-06-03 13:32:33 -0400

    wip

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ server.get('/api', (req, res) => {
     })
 })
 
+server.use((req, res) => {
+    res.status(404).json({
+        message: 'not found sorry'
+    })
+})
+
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
 })
